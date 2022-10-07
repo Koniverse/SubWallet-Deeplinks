7eca12675be125c34f02466ae7e8158217639d81
AnhMTV <maithachvietanh@gmail.com>
2022-10-07 20:16:41 +0700

    Implement simple DApp deeplink generator

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {ChangeEventHandler, EventHandler, MouseEventHandler, useState} from 'react';
+import QRCode from 'react-qr-code';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          SubWallet Deep Links
-        </p>
-      </header>
-    </div>
-  );
+    const [inputUrl, setInputUrl] = useState('');
+    const [deeplink, setDeeplink] = useState('');
+    const onUrlInputChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+        setInputUrl(event.target.value);
+    }
+    const onGenerateDeeplink: MouseEventHandler<HTMLButtonElement> = (event) => {
+        if (inputUrl.startsWith('http')) {
+            setDeeplink(`subwallet://browser?url=${encodeURIComponent(inputUrl)}`);
+        } else {
+            setDeeplink('');
+        }
+    }
+    return (
+        <div className="app-wrapper">
+            <div className="app-container">
+                <h3 className="main-title">
+                    SubWallet Deep Link Generator
+                </h3>
+                <div className="app-content">
+                    <input type="text" className="input-link" value={inputUrl} onChange={onUrlInputChange}
+                           placeholder="DApp link (https://...)"/>
+                    <button className="btn btn-primary generate-btn" onClick={onGenerateDeeplink}>Generate URL</button>
+                    {deeplink !== '' && <>
+                        <a className="deeplink-result" href={deeplink}>{deeplink}</a>
+                        <div className='qr-wrapper'>
+                            <QRCode
+                                size={256}
+                                style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+                                value={deeplink}
+                                viewBox={`0 0 256 256`}
+                            />
+                        </div>
+                    </>}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
